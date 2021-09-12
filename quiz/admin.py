cecd974db912712c35a90ec3420a5f97fa1f91dd
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-09-13 00:23:34 +0530

    Arcane

--- quiz/admin.py
+++ quiz/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Stage_1, StageTwo
 # Register your models here.
 
-admin.site.site_header = "Qriosity 2.0"
+admin.site.site_header = "Arcane"
 admin.site.site_title = "User Admin Area"
-admin.site.index_title = "Welcome to the Qriosity admin area"
+admin.site.index_title = "Welcome to the Arcane admin area"
 
 
 admin.site.register(Stage_1)
