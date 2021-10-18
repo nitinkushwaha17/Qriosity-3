76d8dfc1b4eb57ee720e5dfbd528a30d699027cd
sa-y-an <sayan.ee.nitd@gmail.com>
2021-10-18 17:55:48 +0530

    Cloudfare Khanki Maagi Banchod MC

--- Qriosity/settings.py
+++ Qriosity/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['herokuapp.com']
+ALLOWED_HOSTS = ['herokuapp.com', 'arcane.ieeesbnitdgp.com']
 
 
 # Application definition
